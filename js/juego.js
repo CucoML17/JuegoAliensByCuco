1d5868191035b0e1fdc80ace46766ae918887060
Carlos Abel Mejía López <46359061+CucoML17@users.noreply.github.com>
2024-11-01 14:10:51 -0600

    Update juego.js

--- js/juego.js
+++ js/juego.js
@@ -796,7 +796,7 @@ function actualizarJuego() {
     ctx.textAlign = 'left'; 
 
     
-    if (puntaje >= 1 && !yaAcabo) {
+    if (puntaje >= 100 && !yaAcabo) {
         juegoEnPausa = true;
         yaAcabo = true;
         pausarGeneradores();
